b87faa439f372cb32c955aa77c0e3be2bd43a554
sominsomin <simon.fe.zoller@gmail.com>
2024-07-11 09:54:45 +0200

    readme changes 4

--- README.md
+++ README.md
@@ -105,7 +105,7 @@ b: b color value\
 waveform: waveform of the oscillator/generator\
 offset: phase offset in the rgb values of the oscillator/generator\
 scale: scale the oscillator/generator/input\
-scale_fract: scale the oscillator/generator with repeating tile effect\
+scale_fract: scale the oscillator/generator with a repeating tile effect\
 n: number of corners of the polygon shape\
 xOff: offset in x-direction\
 yOff: offset in y-direction\
--- src/GUI/ShaderValue.js
+++ src/GUI/ShaderValue.js
@@ -30,7 +30,7 @@ display_texts = {
     'waveform': 'waveform of the oscillator/generator',
     'offset': 'phase offset in the rgb values of the oscillator/generator',
     'scale': 'scale the oscillator/generator/input',
-    'scale_fract': 'scale the oscillator/generator with repeating tile effect',
+    'scale_fract': 'scale the oscillator/generator with a repeating tile effect',
     'n': 'number of corners of the polygon shape',
     'xOff': 'offset in x-direction',
     'yOff': 'offset in y-direction',
